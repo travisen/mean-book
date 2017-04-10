4c11c12626bf8460c8534eba1f358dcdef56d67b
travisen <travis.nykaza@gmail.com>
2017-04-10 17:47:41 -0400

    finish homepage templating

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,33 @@
 /* Get 'home' page */
 module.exports.homelist = function(req, res){
-	res.render('locations-list', {title: 'Home'});
+	res.render('locations-list', {
+        title: 'Loc8r - find a place to work with WIFI.',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find awesome places to work with wifi near you!!'
+        },
+        sidebar: ' Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with cofee, cake or a pint?? Let Loc8r help you find the place you\'re looking for.',
+        locations: [{
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '100m'
+        }, {
+            name: 'Ninas',
+            address: '1111 Dunmore, PA',
+            rating: 5,
+            facilities: ['Pizza', 'Wings', 'Premium wifi'],
+            distance: '1000m'
+        },{
+            name: 'Boss',
+            address: 'Boss St, PA, 1850BOSS',
+            rating: 3,
+            facilities: ['Boss Food', 'Regular FOOD', 'bad wings',
+            'cat fancy'],
+            distance: '200m'            
+        }]
+    });
 };
 
 /* Get 'Location info' page */
