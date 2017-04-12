d8fde3ab3ea0fcc8bf19b7c3af7c237eebfe451b
travisen <travis.nykaza@gmail.com>
2017-04-11 20:26:59 -0400

    Moved other pages' content to the controllers

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -22,7 +22,7 @@ module.exports.homelist = function(req, res){
         },{
             name: 'Boss',
             address: 'Boss St, PA, 1850BOSS',
-            rating: 3,
+            rating: 1,
             facilities: ['Boss Food', 'Regular FOOD', 'bad wings',
             'cat fancy'],
             distance: '200m'            
@@ -30,13 +30,60 @@ module.exports.homelist = function(req, res){
     });
 };
 
-/* Get 'Location info' page */
-module.exports.locationInfo = function(req, res){
-	res.render('location-info', {title: 'Location info'});
+/* GET 'Location info' page */
+module.exports.locationInfo = function(req, res) {
+    res.render('location-info', {
+        title: 'Starcups',
+        pageHeader: {
+            title: 'Starcups'
+        },
+        sidebar: {
+            context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+            callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+        },
+        location: {
+            name: 'Starcups',
+            address: '999 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            coords: {
+                lat: 41.443582,
+                lng: -75.674543
+            },
+            openingTimes: [{
+                days: 'Monday - Friday',
+                opening: '7:00am',
+                closing: '7:00pm',
+                closed: false
+            }, {
+                days: 'Saturday',
+                opening: '8:00am',
+                closing: '5:00pm',
+                closed: false
+            }, {
+                days: 'Sunday',
+                closed: true
+            }],
+            reviews: [{
+                author: 'Simon Holmes',
+                rating: 5,
+                timestamp: '16 July 2013',
+                reviewText: 'What a great place. I can\'t say enough good things about it.'
+            }, {
+                author: 'Charlie Chaplin',
+                rating: 3,
+                timestamp: '16 June 2013',
+                reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+            }]
+        }
+    });
 };
 
 /* Get 'Add review' pages */
 module.exports.addReview = function(req, res){
-	res.render('location-review-form', {title: 'Add review'});
+	res.render('location-review-form', {
+        title: 'Review Starcups on Loc8r',
+        pageHeader: { title: 'Review starcups' }
+    });
 };
 
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,8 @@
 /* GET about page */
 module.exports.about = function(req, res){
-	res.render('generic-text', {title: 'About'});
+	res.render('generic-text', {
+        title: 'About',
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Ut condimentum lacinia felis, eget pretium urna accumsan a. Vivamus posuere nunc nunc, quis pharetra justo iaculis eget. Morbi dictum odio fermentum venenatis ultrices. Praesent luctus eros id magna laoreet, quis fringilla mi vestibulum.\n\n Aliquam tempor, nunc ac laoreet sagittis, enim tellus condimentum mauris, id vulputate ex ex nec ex. Fusce semper consectetur ligula, eget mollis est imperdiet sit amet. Integer efficitur massa tortor, iaculis accumsan mi efficitur vel. Donec placerat consectetur diam, sodales faucibus elit facilisis nec. \n\nNam fermentum, tortor quis feugiat porttitor, turpis eros laoreet nisl, quis fringilla massa libero a ipsum. Donec congue nisl id nisi venenatis, eget porttitor nisi congue. Mauris in nisi non justo maximus vehicula imperdiet ut magna. Aliquam pharetra molestie nisi. Vestibulum faucibus magna eu consectetur condimentum."
+    });
 };
+
